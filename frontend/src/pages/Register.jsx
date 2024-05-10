8ef1ec7fdd8fea22ab3c18727678c4f29a1c313f
SnowGloves <112101618+SnowGloves@users.noreply.github.com>
2024-05-10 16:40:51 +0700

    sign in and register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Form } from 'react-bootstrap'
 import Logo from '../assets/logo-no-background.png'
+import '../styles/register.css'
 
 const Register = () => {
   return (
-    <div>
-      <img src={Logo} alt="Next Chapter logo" className='h-44' />
-      <header className='font-bold'>Register</header>
+    <div id='container'>
+      <img src={Logo} id='register-logo' alt="Next Chapter logo" className='h-44' />
+      <header id='register-label' className='font-bold'>Register</header>
       <div>
-        <Form id='container'>
+        <Form>
           <input type='text' name='username' placeholder='username' />
           <br />
           <input type='password' name='password' placeholder='password' />
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -3,7 +3,7 @@ import { Form } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../hooks/AuthProvider'
 import Logo from '../assets/logo-no-background.png'
-import "../index.css"
+import '../styles/signin.css'
 
 const SignIn = () => {
   const [username, setUsername] = useState('')
@@ -21,26 +21,28 @@ const SignIn = () => {
   }
 
   return (
-    <div>
-      <img src={Logo} alt="Next Chapter logo" className='h-44' />
-      <header className='font-bold'>Sign in to your account</header>
-      <div>
-        <Form id ='content-box'onSubmit={handleSubmit}>
-          <input type='text' name='username' value={username} onChange={(e) => setUsername(e.target.value)} placeholder='username' />
-          <br />
-          <input type='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' />
-          <div>
-            <button type="submit" className='font-bold'>Sign In</button>
-          </div>
-          <div>
+    <div id='container-all'>
+      <div id='content-box'>
+        <img src={Logo} id='login-logo' alt="Next Chapter logo" className='h-44' />
+        <header id='login-label' className='font-bold'>Sign in to your account</header>
+        <div>
+          <Form onSubmit={handleSubmit}>
+            <input type='text' name='username' value={username} onChange={(e) => setUsername(e.target.value)} placeholder='username' />
+            <br />
+            <input type='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' />
             <div>
-              New User?
-              <Link to={`/register`} className='font-bold'> Register</Link>
+              <button id='submit-button' type="submit" className='font-bold'>Sign In</button>
             </div>
-          </div>
-        </Form>
+            <div>
+              <div>
+                New User?
+                <Link to={`/register`} className='font-bold'> Register</Link>
+              </div>
+            </div>
+          </Form>
+        </div>
       </div>
-    </div>
+      </div>
   )
 }
 
